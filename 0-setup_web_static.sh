3f418b12543b4c8c62ec885d01dc6c5a5e4f8800
Amanbel <amanuel11belachew@gmail.com>
2023-07-11 12:09:54 +0300

    automated setup

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,9 +1,8 @@
 #!/usr/bin/env bash
 # setting up the web servers for deplayment of the static page
-
-sudo apt-get -y update
-sudo apt-get -y upgrade
-sudo apt-get -y install nginx
+sudo apt-get update -y
+sudo apt-get upgrade -y
+sudo apt-get install nginx -y
 sudo mkdir -p /data/web_static/releases/test /data/web_static/shared
 echo -e "<html>\n<head></head>\n<body>\ntest page for nginx\n</body>\n</html>" | sudo tee /data/web_static/releases/test/index.html
 sudo ln -sf /data/web_static/releases/test/ /date/web_static/current
