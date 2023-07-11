251e1ca83ddba84c1d4563e8f6db1192ff7fe48d
Amanbel <amanuel11belachew@gmail.com>
2023-07-11 12:52:01 +0300

    automated setup

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -5,7 +5,7 @@ sudo apt-get upgrade -y
 sudo apt-get install nginx -y
 sudo mkdir -p /data/web_static/releases/test /data/web_static/shared
 echo -e "<html>\n<head></head>\n<body>\ntest page for nginx\n</body>\n</html>" | sudo tee /data/web_static/releases/test/index.html
-sudo ln -sf /data/web_static/releases/test/ /date/web_static/current
+sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
 sudo chown ubuntu:ubuntu -hR /data/
 sudo sed -i '38i\\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}\n' /etc/nginx/sites-available/default
 sudo service nginx restart
