b06a2fe78bb7ca801713dd48033e9928c597a521
Amanbel <amanuel11belachew@gmail.com>
2023-07-28 12:39:27 +0300

    web_framework

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,12 +1,18 @@
 #!/usr/bin/python3
+""" using flask to run a
+local server to serve text """
 
 from flask import Flask
 
 app = Flask(__name__)
 
+
 @app.route('/')
 def hello_hbtn():
+    """ function that serves
+    the text Hello HBNB! """
     return "Hello HBNB!"
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="5000")
